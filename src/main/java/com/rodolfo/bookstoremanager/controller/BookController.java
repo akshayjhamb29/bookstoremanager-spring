0e70f6408bf07ba35ec3e034d6d364ab1222a3ba
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2020-12-29 11:45:22 -0300

    Sonar setup and undo reset

--- src/main/java/com/rodolfo/bookstoremanager/controller/BookController.java
+++ src/main/java/com/rodolfo/bookstoremanager/controller/BookController.java
@@ -1,8 +1,5 @@
 package com.rodolfo.bookstoremanager.controller;
 
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/books")
 public class BookController {
 
-    @ApiOperation(value = "Returns an example of Hello World")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Success method return")
-    })
     @GetMapping
     public String hello() {
         return "Hello Bookstore Manager";
